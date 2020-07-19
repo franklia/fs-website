9d4427fceab16fef907fe39cd6f777500dbf2912
Frank Liardet <frank.liardet@gmail.com>
2020-07-19 17:44:43 +1000

    Updates

---
+++ precache-manifest.91dd6ee3d17cd9d1be9c9e78984c414f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eab8aa75472ad8aa40b4c75387923fe",
+    "url": "/fs-website/index.html"
+  },
+  {
+    "revision": "acb0610cc76447886c3c",
+    "url": "/fs-website/static/js/2.45793bf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fs-website/static/js/2.45793bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b7b1367786b17bad73",
+    "url": "/fs-website/static/js/main.9ee5a30c.chunk.js"
+  },
+  {
+    "revision": "258e8a722dd2f9b9fa30",
+    "url": "/fs-website/static/js/runtime-main.951fedc4.js"
+  },
+  {
+    "revision": "ded212230d41a5312db3f8a7f1ee73f8",
+    "url": "/fs-website/static/media/CTAFrank.ded21223.png"
+  },
+  {
+    "revision": "3afbc8a15e36fe5a19356cc645785d81",
+    "url": "/fs-website/static/media/frank-1.3afbc8a1.png"
+  },
+  {
+    "revision": "a9e8ca4d91d099556fdb9981a2e708f3",
+    "url": "/fs-website/static/media/point.a9e8ca4d.png"
+  },
+  {
+    "revision": "079f2fc5fd1d6726a41f92a4f9e1a384",
+    "url": "/fs-website/static/media/whistle.079f2fc5.png"
+  }
+]);
